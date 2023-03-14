3d46c32591fbc8865da2458c7473eaf9a2146461
wasswarichard <wasswarichard55@gmail.com>
2023-03-14 10:06:42 +0300

    login module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LoginModule } from './login/login.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, LoginModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: ['/../dist/**/*.entity.js'],
+    }),
+    UsersModule,
+    LoginModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,10 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity('user')
 export class User {
   @ApiProperty()
+  @PrimaryGeneratedColumn()
   id: number;
+
   @ApiProperty()
+  @Column()
   name: string;
+
+  @Column()
   username: string;
+
+  @Column()
   password: string;
 }
