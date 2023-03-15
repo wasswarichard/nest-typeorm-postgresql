1747422a2b251a193e9e8635228503bee7acb2e0
wasswarichard <wasswarichard55@gmail.com>
2023-03-15 09:02:26 +0300

    server side events

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { interval, map, Observable } from 'rxjs';
 
-@Controller()
+interface MessageEvent {
+  data: string | object;
+}
+
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('event')
+  sendEvent(): Observable<MessageEvent> {
+    return interval(1000).pipe(
+      map((num: number) => ({
+        data: 'hello' + num,
+      })),
+    );
   }
 }
