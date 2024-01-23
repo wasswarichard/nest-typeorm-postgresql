61231dfc040335383c12135115e13ac0862e04aa
wasswarichard <wasswarichard55@gmail.com>
2024-01-23 09:52:15 +0300

    update readme

---
+++ db/data-source.ts
@@ -0,0 +1,32 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'sqlite',
+  database: 'db.sqlite',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/db/migrations/*.js']
+};
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
+
+
+
+// {
+//   type: 'postgres',
+//     host: process.env.POSTGRES_HOST,
+//   port: parseInt(<string>process.env.POSTGRES_PORT),
+//   username: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DATABASE,
+//   autoLoadEntities: true,
+//   synchronize: true,
+//   entities: ['/../dist/**/*.entity.js'],
+//   // migrations: ['dist/src/db/migrations/*.js'],
+//   // cli: {
+//   //   migrationsDir: 'src/db/migrations'
+//   // },
+// }
+
+// commands
+// npm run migration:generate --db/migrations/NewMigration
+//npm run migration:run
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,25 +6,12 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PetModule } from './pet/pet.module';
+import { dataSourceOptions } from '../db/data-source';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true,
-      entities: ['/../dist/**/*.entity.js'],
-      // migrations: ['dist/src/db/migrations/*.js'],
-      // cli: {
-      //   migrationsDir: 'src/db/migrations'
-      // },
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     UsersModule,
     AuthModule,
     PetModule,
